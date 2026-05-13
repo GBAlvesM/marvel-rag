f52736e4c95bbd448e1c9514147c75e06447db9e
GBAlvesM <gabriel.miguel@sptech.school>
2026-05-13 00:28:28 -0300

    Iniciando a estrutura de requisições para a API Comicvine

--- src/ingestion/fetcher.py
+++ src/ingestion/fetcher.py
@@ -1,3 +1,28 @@
+from dotenv import load_dotenv
+
+import requests
+import json
+import os
+
+load_dotenv()
+
 def buscar_personagens(limite = 10):
-    print("Buscando personagens...")
-    return
+    API_COMICVINE = os.getenv("COMICVINE_API_KEY")
+
+    if (API_COMICVINE is None):
+        print("Chave API Comicvine não encontrada")
+        return
+    else:
+        print("Buscando personagens...")
+        URL_PERSONAGENS = f"https://comicvine.gamespot.com/api/characters/?api_key={API_COMICVINE}&format=json&limit={limite}"
+
+        resposta = requests.get(URL_PERSONAGENS, headers={"User-Agent": "marvel-rag"})
+        resposta = resposta.json()
+
+        return
+    
+
+if __name__ == "__main__":
+    buscar_personagens()
+    
+
